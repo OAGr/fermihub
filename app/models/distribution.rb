9e45bd99899410597b8fead1b18075e78deedf91
Ozzie Gooen <Ozzie@BowLabs.org>
2013-05-11 23:53:16 -0700

    changed table names, getting rspec set up

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -1,4 +1,6 @@
 class Distribution < ActiveRecord::Base
+  include DistributionStats
+
   attr_accessible :spread, :mean, :wideness
   has_and_belongs_to_many :outputs, :class_name => "Operation"
   belongs_to :operation
