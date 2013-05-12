ad4bbc7591761c8621c06690e124a884856087f6
Ozzie Gooen <Ozzie@BowLabs.org>
2013-05-11 21:16:43 -0700

    fermi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fermi::Application.config.secret_token = '99a6d02a166a9d8071457e23478cbd374954495a62bd67e6eb34fd41e32d7ab89b772c11822e6bf2e90c8125423ee6f06a4aa9db86b39b4f2be26b5dcf3e59f0'
