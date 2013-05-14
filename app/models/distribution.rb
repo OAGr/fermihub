a248bf6987fbad27b9ef43be087d1a4ef1b03242
Ozzie Gooen <Ozzie@BowLabs.org>
2013-05-14 03:39:32 -0700

    still moving libs around

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -1,4 +1,4 @@
-require 'Dis'
+require 'dis'
 class Distribution < ActiveRecord::Base
   has_and_belongs_to_many :outputs, :class_name => "Operation"
   attr_accessible :name, :mean, :spread, :wideness
