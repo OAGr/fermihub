f1403ddd568c8ff47b345861812e419e1a17bcea
Ozzie Gooen <Ozzie@BowLabs.org>
2013-05-14 01:40:39 -0700

    made horizontal split, vsplit option

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   def home
     @operations = Operation.all
     @distributions = Distribution.all
+    @dependents = Dependent.all
+    @independents = Independent.all
   end
 end 
